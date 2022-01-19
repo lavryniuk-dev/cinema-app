d516e7348078191c98d33f69a93a2b7f84e47428
Illia Lavryniuk <lavryniuk.dev@gmail.com>
2022-01-19 18:21:34 +0200

    added spring to the project

--- src/main/java/cinema/model/Order.java
+++ src/main/java/cinema/model/Order.java
@@ -2,43 +2,23 @@ package cinema.model;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.Table;
 
 @Entity
-@Table(name = "orders")
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @OneToMany
-    @JoinTable(name = "orders_tickets",
-            joinColumns = @JoinColumn(name = "order_id"),
-            inverseJoinColumns = @JoinColumn(name = "ticket_id"))
     private List<Ticket> tickets;
-    @Column(name = "order_date")
-    private LocalDateTime orderDate;
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
+    private LocalDateTime orderTime;
+    @ManyToOne
     private User user;
-
-    public Order() {
-    }
-
-    public Order(List<Ticket> tickets, LocalDateTime orderDate, User user) {
-        this.tickets = tickets;
-        this.orderDate = orderDate;
-        this.user = user;
-    }
 
     public Long getId() {
         return id;
@@ -56,12 +36,12 @@ public class Order {
         this.tickets = tickets;
     }
 
-    public LocalDateTime getOrderDate() {
-        return orderDate;
+    public LocalDateTime getOrderTime() {
+        return orderTime;
     }
 
-    public void setOrderDate(LocalDateTime orderDate) {
-        this.orderDate = orderDate;
+    public void setOrderTime(LocalDateTime orderTime) {
+        this.orderTime = orderTime;
     }
 
     public User getUser() {
@@ -77,8 +57,7 @@ public class Order {
         return "Order{"
                 + "id=" + id
                 + ", tickets=" + tickets
-                + ", orderDate=" + orderDate
-                + ", user=" + user
-                + '}';
+                + ", orderTime=" + orderTime
+                + ", user=" + user + '}';
     }
 }
